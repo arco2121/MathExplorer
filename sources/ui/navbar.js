17f7913e03e089d0f0e3c45cfb7779ed7c642e1e
Your Name <you@example.com>
2025-11-08 10:50:29 +0100

    Icons

--- sources/ui/navbar.js
+++ sources/ui/navbar.js
@@ -5,7 +5,7 @@ document.body.querySelector("ui").innerHTML += `
         <h3>Math Explorer</h3>
     </row>
     <row>
-        <button><object type="image/svg+xml" data="./img/upload.svg"></object></button>
+        <button onclick="location.href='/upload'"><object type="image/svg+xml" data="./img/upload.svg"></object></button>
         <button id="colorthemeBt"><object type="image/svg+xml" data="./img/theme.svg"></object></button>
     </row>
 </nav>
